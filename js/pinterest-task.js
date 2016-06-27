cc0b3fd2683a7f8cdfb78850b2edbe0479f8b128
Gabry <gabriele.cimato@gmail.com>
2016-06-27 01:47:03 -0700

    Refactor: typos on intro

--- js/pinterest-task.js
+++ js/pinterest-task.js
@@ -36,14 +36,15 @@ $('#pinterest-auth').on('click', function() {
 				the API endpoint adding the token. This url is already set up to
 				retrieve a user board name, url, image and id.
 			*/
-			$.getJSON( "https://api.pinterest.com/v1/me/boards/?access_token=" + res.session.accessToken +"&fields=id%2Cname%2Curl%2Cimage", function( res ) {
+			var accessToken = res.session.accessToken;
+			$.getJSON( "https://api.pinterest.com/v1/me/boards/?access_token=" + accessToken +"&fields=id%2Cname%2Curl%2Cimage", function( userBoards ) {
 				/*
 					Once the request goes through we are able to display the user data.
 					The API returns an array of Objecs, each object represents a user board.
 					In this case we loop through each board of the user and display the data
 					we need.
 				*/
-				res.data.forEach( function (board) {
+				userBoards.data.forEach( function (board) {
 					$('.user-content').append('<div class="user-board"><h3>' + board.name + '</h3><img alt="board image" src="' + board.image['60x60'].url + '"></img><a href="' + board.url + '" target="_blank"><button class="btn">Link</button></div>');
 				})
 			});
